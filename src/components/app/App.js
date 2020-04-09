732307803b8551daa11d51c20d1640b4d4e6ef39
Emmanuel Barroga <emmanuelbarroga@gmail.com>
2020-04-09 15:43:03 -0700

    Searchbar component
    Initial searcbar component implementation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import Search from '../Search';
+
 function App() {
+
+  const searchFunction = (query) => {
+    console.log("Searching for " + query);
+
+    fetch('http://www.omdbapi.com/?s='+query+'&apikey=2e759236')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Search onSubmitSearch={searchFunction}/>
         <h1>v18 Geckos - Team 1!</h1>
       </header>
     </div>
