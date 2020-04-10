94c172f53f3ae8d29f6333d0b98351812ff44891
jbonilla8 <jalisabonilla10@gmail.com>
2020-04-10 11:56:50 -0500

    added hero component and image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import HeroImg from '../images/bloodshot.jpg';
 
-function Hero() {
+const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <HeroImage />
+  );
+};
 
 export default Hero;
+
+const HeroImage = styled.div`
+  background-image: url(${HeroImg});
+  height: 65vh;
+  width: 100vw;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media (max-width: 800px) {
+    height: 50vh;
+  }
+
+  @media (max-width: 600px) {
+    height: 33vh;
+  }
+  
+`;
