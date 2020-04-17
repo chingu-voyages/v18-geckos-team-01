a628f415104a3122795e4e803429604fbc01b668
jbonilla8 <jalisabonilla10@gmail.com>
2020-04-16 19:17:48 -0500

    add SignIn modal component
    Still needs an onClick handler and changes for it to be responsive

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SignIn = () => {
+const SignIn = (props) => {
   return (
-    <SignInContainer>
-      <h1>Sign In To Your Account</h1>
+    <SignInContainer showSignInModal={props.showSignInModal}>
       <SignInForm>
+        <h1>Sign In To Your Account</h1>
         <input type="text" placeholder="Email" />
         <input type="text" placeholder="Password" />
         <SignInButton>Sign In</SignInButton>
         <h3>
           Don't have an account? <span>Create</span>
         </h3>
+        <h2>
+          Or sign in with <span>Facebook</span>
+        </h2>
       </SignInForm>
     </SignInContainer>
   );
@@ -20,9 +23,10 @@ const SignIn = () => {
 export default SignIn;
 
 const SignInContainer = styled.div`
+  display: ${props => (props.showSignInModal ? 'block' : 'none')};
   position: absolute;
   top: 25%;
-  left: 25%;
+  left: 33%;
   width: 450px;
   height: 450px;
   background: #000;
@@ -30,6 +34,17 @@ const SignInContainer = styled.div`
   box-sizing: border-box;
   border-radius: 25px;
   z-index: 1;
+`;
+
+const SignInForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-radius: 25px;
+  height: 100%;
+  width: 100%;
 
   h1 {
     font-family: Poppins;
@@ -37,21 +52,37 @@ const SignInContainer = styled.div`
     font-size: 28px;
     line-height: 45px;
     color: #ffc107;
-    text-align: center;
   }
 
   h3 {
     color: white;
-    font-weight: 300;
+    font-weight: 400;
+
     span {
       cursor: pointer;
       color: #ffc107;
+
+      &:hover {
+        color: #cc8e00;
+      }
     }
   }
-`;
 
-const SignInForm = styled.div`
-  text-align: center;
+  h2 {
+    color: white;
+    font-size: 22px;
+    font-weight: 400;
+
+    span {
+      cursor: pointer;
+      color: #3b5998;
+      font-weight: 600;
+
+      &:hover {
+        color: #22407f;
+      }
+    }
+  }
 
   input {
     font-size: 20px;
@@ -68,13 +99,20 @@ const SignInForm = styled.div`
 `;
 
 const SignInButton = styled.button`
+  cursor: pointer;
   font-size: 20px;
+  font-weight: 500;
   letter-spacing: 0.05em;
   text-transform: uppercase;
   color: white;
   background: #0087a5;
   border-radius: 25px;
-  width: 70%;
+  width: 72%;
   height: 3rem;
   border-style: none;
+  margin-bottom: 1rem;
+
+  &:hover {
+    background: #006e8c;
+  }
 `;
