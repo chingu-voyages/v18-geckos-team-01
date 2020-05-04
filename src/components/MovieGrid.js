d25728f1281eafc9b2ace841ff36c4476040e0a7
jbonilla8 <jalisabonilla10@gmail.com>
2020-05-04 15:07:25 -0400

    responsive grid work

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Star from '../images/star.svg';
+import Plus from '../images/plus.svg';
 import styled from 'styled-components';
 
 const MovieCard = props => {
@@ -13,17 +15,15 @@ const MovieCard = props => {
         />
       </Link>
       <CardFooter>
+        <StarIcon src={Star} alt={'star logo for imdb rating'} />
+        <span>{props.imdbRating}</span>
         <CardText>
           {props.title} ({props.year})
         </CardText>
-        <span
-          style={{ color: 'white' }}
-          onClick={() => {
-            props.onSaveMovie(props.title, props.id);
-          }}
-        >
-          Add to Watchlist
-        </span>
+        <AddToWatchlist
+          src={Plus}
+          alt={'plus sign to add movie to watchlist'}
+        />
       </CardFooter>
     </Card>
   );
@@ -66,6 +66,7 @@ const MovieGrid = props => {
           poster={element.Poster}
           year={element.Year}
           title={element.Title}
+          imdbRating={element.imdbRating}
           id={element.imdbID}
           onSaveMovie={props.onSaveMovie}
         />
@@ -80,28 +81,44 @@ const MovieGridCards = styled.div`
   display: grid;
   justify-content: space-around;
   grid-template-columns: repeat(4, auto);
+  grid-auto-rows: 400px;
 
   @media (max-width: 1140px) {
-    grid-template-columns: auto auto auto;
+    grid-template-columns: repeat(3, auto);
   }
 
   @media only screen and (max-width: 900px) {
-    grid-template-columns: auto auto;
+    grid-template-columns: repeat(2, auto);
   }
 
-  @media only screen and (max-width: 600px) {
-    width: 180px;
-    height: 230px;
+  @media only screen and (max-width: 499px) {
+    grid-template-columns: repeat(2, auto);
+    grid-auto-rows: 310px;
+  }
+
+  @media only screen and (max-width: 320px) {
+    grid-template-columns: auto;
+    grid-auto-rows: 340px;
   }
 `;
 
 const Card = styled.div`
   flex-direction: column;
   border: 2px solid #f9c132;
-  border-radius: 25px;
+  border-radius: 25px 25px 0px 0px;
   width: 220px;
   height: 260px;
   margin-top: 20px;
+
+  @media only screen and (max-width: 499px) {
+    width: 160px;
+    height: 200px;
+  }
+
+  @media only screen and (max-width: 320px) {
+    width: 200px;
+    height: 240px;
+  }
 `;
 
 const Poster = styled.img`
@@ -112,19 +129,63 @@ const Poster = styled.img`
   cursor: pointer;
 `;
 
-const CardText = styled.div`
-  padding: 10px 15px;
-  flex-wrap: wrap;
-  color: white;
-  border-bottom-right-radius: 25px;
-  border-bottom-left-radius: 25px;
-  cursor: pointer;
-`;
-
 const CardFooter = styled.div`
+  display: grid;
+  grid-template-columns: 80% 20%;
+  grid-template-rows: 30% 70%;
   background-color: black;
   border: 2px solid #f9c132;
   border-top: none;
   border-radius: 0px 0px 25px 25px;
-  height: 60px;
+  height: 90px;
+  color: white;
+  margin-top: -7px;
+  margin-left: -2px;
+  width: inherit;
+
+  @media only screen and (max-width: 499px) {
+    height: 60px;
+    font-size: 0.8rem;
+  }
+`;
+
+const StarIcon = styled.img`
+  grid-column: 1/2;
+  grid-row: 1/2;
+  width: 1.2rem;
+  height: 1.2rem;
+  padding: 0.5rem 0.5rem 0rem 0.5rem;
+
+  @media only screen and (max-width: 499px) {
+    width: 1rem;
+    height: 1rem;
+  }
+`;
+
+const CardText = styled.div`
+  grid-column: 1/2;
+  grid-row: 2/3;
+  padding: 10px 0px 0px 10px;
+  flex-wrap: wrap;
+  font-weight: 600;
+  border-bottom-right-radius: 25px;
+  border-bottom-left-radius: 25px;
+  cursor: pointer;
+  align-self: start;
+`;
+
+const AddToWatchlist = styled.img`
+  grid-column: 2/3;
+  grid-row: 2/3;
+  width: 1.8rem;
+  height: 1.8rem;
+  cursor: pointer;
+  align-self: end;
+  margin-bottom: 0.5rem;
+  justify-self: center;
+
+  @media only screen and (max-width: 499px) {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
 `;
